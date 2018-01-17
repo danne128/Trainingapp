ea6d2c74ddb883f069c0d899999d9042497e3530
Daniel Trondsen Wallin <danne_tw@hotmail.com>
2018-01-17 11:11:53 +0100

    Added tableview and cell

---
+++ Trainingapp/WorkOutsTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  WorkOutsTableViewCell.swift
+//  Trainingapp
+//
+//  Created by Daniel Trondsen Wallin on 2018-01-17.
+//  Copyright © 2018 Daniel Trondsen Wallin. All rights reserved.
+//
+
+import UIKit
+
+class WorkOutsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var workOutNameLabel: UILabel!
+    @IBOutlet weak var amountOfWorkoutsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
