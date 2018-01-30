6080b6ea2049c30230df8c76fdf12930912524e4
Daniel Trondsen Wallin <danne_tw@hotmail.com>
2018-01-30 14:14:40 +0100

    removed commented text

--- Trainingapp/WorkOutsTableViewController.swift
+++ Trainingapp/WorkOutsTableViewController.swift
@@ -34,58 +34,6 @@ class WorkOutsTableViewController: UITableViewController {
         
         self.navigationItem.hidesBackButton = true
         
-        //loadData()
-        
-        /*workOutPath.observeSingleEvent(of: .value) { (snapshot) in
-            let value = snapshot.value as? NSDictionary
-            if value != nil {
-                let keys = value!.allKeys
-                for key in keys {
-                    self.workOuts.append(key as! String)
-                }
-                
-                for exercise in self.workOuts {
-                    self.workOutPath.child(exercise).observeSingleEvent(of: .value, with: { (snapshot2) in
-                        let value2 = snapshot2.value as? NSDictionary
-                        let keys2 = value2!.allKeys
-                        self.amountOfExercises.append(keys2.count)
-                        
-                        if self.amountOfExercises.count == self.workOuts.count {
-                            self.end = true
-                        }
-                        
-                        if self.end == true {
-                            self.tableView.reloadData()
-                        }
-                    })
-                }
-
-            }
-            else {
-                print("Your workout table is empty")
-                let noDataLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.tableView.bounds.size.width, height: self.tableView.bounds.size.height))
-                noDataLabel.font = UIFont(name: noDataLabel.font.fontName, size: 20)
-                noDataLabel.text = "You dont have any workouts yet"
-                noDataLabel.textColor = UIColor.black
-                noDataLabel.textAlignment = .center
-                
-                
-                let addDataButton: UIButton = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
-                addDataButton.backgroundColor = .black
-                addDataButton.setTitle("Add a workout", for: .normal)
-                addDataButton.titleLabel?.font = UIFont(name: (addDataButton.titleLabel?.font.fontName)!, size: 20)
-                addDataButton.titleLabel?.adjustsFontSizeToFitWidth = true
-                addDataButton.frame.origin = CGPoint(x: self.tableView.bounds.size.width*0.25, y: self.tableView.bounds.height*0.5)
-                addDataButton.addTarget(self, action: #selector(self.addDataButtonWasClicked), for: .touchUpInside)
-                
-    
-                self.tableView.backgroundView  = noDataLabel
-                self.tableView.addSubview(addDataButton)
-                self.tableView.separatorStyle  = .none
-                self.tableView.tableFooterView = UIView()
-            }
-        }*/
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
